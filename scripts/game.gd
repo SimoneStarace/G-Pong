f5f41ff360ef7f6e7536387aa06ef668380fb214
Simone Starace <simone.starace93@gmail.com>
2024-02-11 19:37:22 +0100

    Apply reset of ball when is out of bounds

--- scripts/game.gd
+++ scripts/game.gd
@@ -11,12 +11,18 @@ func _ready():
 func _on_ball_exited():
 	# Check if the ball exited on the left or on the right
 	if _ball.global_position.x < 0:
-		reset_ball()
+		reset_ball(Vector2.LEFT)
 	else:
-		reset_ball()
+		reset_ball(Vector2.RIGHT)
 
 ## Resets the ball
-func reset_ball():
+func reset_ball(direction:Vector2):
 	# Put the ball at the center and don't move it.
 	_ball._direction = Vector2.ZERO
-	_ball.reset_position()
+	_ball.reset()
+	# Add a small gap to let the player understand where the ball will launch
+	_ball.global_position += Vector2(-100, 0) if direction == Vector2.LEFT else Vector2(100,0)
+	# Wait for 1.5 seconds
+	await get_tree().create_timer(1.5).timeout
+	# Specify the direction
+	_ball._direction = direction
