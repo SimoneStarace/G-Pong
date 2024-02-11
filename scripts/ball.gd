fb552cac472073a531596c0729d2a26804a40615
Simone Starace <simone.starace93@gmail.com>
2024-02-11 18:04:47 +0100

    Define initial speed as costant

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,11 +1,14 @@
 class_name Ball extends Area2D
 
+## Initial speed of the ball
+const _initial_speed:int = 300
 ## Movement speed of the ball
-@export var _speed:int = 300
-
+@export var _speed:int = _initial_speed
+## Reference to the screen notifier 2D
 @onready var _2d_notifier = $VisibleOnScreenNotifier2D
-
+## Direction of the ball
 var _direction:Vector2 = Vector2.RIGHT
 
 func _process(delta):
+	# Change the position of the ball
 	global_position += delta * _direction * _speed 
