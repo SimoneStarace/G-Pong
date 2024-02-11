c6a1aa92904d733fb54dd71126784e7f53a4c9e6
Simone Starace <simone.starace93@gmail.com>
2024-02-11 19:36:51 +0100

    Reset speed ball inside reset function

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -16,5 +16,6 @@ func _process(delta):
 	global_position += delta * _direction * _speed 
 
 ## Resets the ball position to the center of the screen
-func reset_position():
+func reset():
 	global_position = _center_screen
+	_speed = _initial_speed
