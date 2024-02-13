ac3f8bdb9e094f788ebfcb58df95a06fe54b7707
Simone Starace <simone.starace93@gmail.com>
2024-02-13 20:23:46 +0100

    Use player controller resource in paddle script

--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -1,5 +1,7 @@
 class_name Paddle extends Area2D
 
+## Paddle movement comands
+@export var _player_controller:PlayerController = preload("res://resources/player1_controller.tres")
 ## Movement speed of the paddle
 @export var _speed:int = 100
 ## Hit direction to apply
@@ -16,7 +18,7 @@ func _ready():
 
 func _process(delta):
 	# Take the direction
-	var direction = Input.get_axis("move_up", "move_down")
+	var direction = Input.get_axis(_player_controller.move_up, _player_controller.move_down)
 	# Change the position
 	self.global_position.y += delta * direction * _speed
 	self.global_position.y = global_position.clamp(Vector2(0,_size_y),Vector2(0,_diff)).y
