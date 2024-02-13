cbb7c05e021c0e9aba2b3b19429e4c569b00752e
Simone Starace <simone.starace93@gmail.com>
2024-02-13 20:19:37 +0100

    Define PlayerController resources

---
+++ scripts/resources/player_controller.gd
@@ -0,0 +1,5 @@
+class_name PlayerController extends Resource
+## Class that specify the name of the commands for moving the Paddle
+
+@export var move_up: String = "player1_move_up"
+@export var move_down: String = "player1_move_down"
