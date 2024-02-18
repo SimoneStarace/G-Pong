77d332d0cbefecd44b0611a0b51dd4132d1a9094
Simone Starace <simone.starace93@gmail.com>
2024-02-18 10:26:35 +0100

    Define Score Menu scene

---
+++ scripts/UI/score_menu.gd
@@ -0,0 +1,17 @@
+class_name ScoreMenu extends Control
+
+## Reference to the score labels
+var _score_labels:Array[Label]
+
+func _ready():
+	# Get all the Label nodes
+	for sl in get_children():
+		if sl is Label:
+			_score_labels.append(sl)
+
+## Sets the text to one of the score labels
+func set_score_value(index:int, value:int):
+	if index < len(_score_labels):
+		_score_labels[index].text = str(value)
+	else:
+		push_warning("Index out of range")
