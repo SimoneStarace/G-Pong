0d900de96fbd1d052930275dee09566b6d86a839
Simone Starace <simone.starace93@gmail.com>
2024-02-18 18:13:18 +0100

    Add OptionManager singleton

---
+++ scripts/managers/option_manager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+@export_range(1,100) var _max_score:int = 10
+@export_category("Colors")
+@export_color_no_alpha var _left_paddle_color = Color.WHITE
+@export_color_no_alpha var _right_paddle_color = Color.WHITE
+@export_color_no_alpha var _background_color = Color.BLACK
