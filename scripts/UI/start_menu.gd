a5f2d585337dd93e5c3ecdd16b590e2b4dea6ffe
Simone Starace <simone.starace93@gmail.com>
2024-02-18 19:17:26 +0100

    Access Option menu from start menu

--- scripts/UI/start_menu.gd
+++ scripts/UI/start_menu.gd
@@ -2,3 +2,6 @@ class_name StartMenu extends Control
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_options_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/UI/option_menu.tscn")
