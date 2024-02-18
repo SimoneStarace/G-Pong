ef9196e8f79dead4d6e18215a3422d77d6d4e959
Simone Starace <simone.starace93@gmail.com>
2024-02-18 11:46:19 +0100

    Add buttons on win screen scene

--- scripts/UI/win_screen.gd
+++ scripts/UI/win_screen.gd
@@ -1,7 +1,17 @@
 class_name WinScreen extends Control
 
+## Label to show the winner player
 @onready var _winner_label:Label = $WinnerLabel
 
+func _ready():
+	# Connect the button signals with lambda functions
+	$ReplayButton.pressed.connect(func():
+		get_tree().reload_current_scene()
+	)
+	$MenuButton.pressed.connect(func():
+		get_tree().change_scene_to_file("res://scenes/UI/start_menu.tscn")
+	)
+
 ## Sets the winner label text
-func set_winner_text(text:String):
+func set_winner_text(text:String) -> void:
 	_winner_label.text = text
