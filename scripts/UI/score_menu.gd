affc7933581c3adedc9d4e1a8c625fa189ad275b
Simone Starace <simone.starace93@gmail.com>
2024-02-18 11:19:35 +0100

    Use feature tags to print warning message

--- scripts/UI/score_menu.gd
+++ scripts/UI/score_menu.gd
@@ -13,5 +13,5 @@ func _ready():
 func set_score_value(index:int, value:int):
 	if index < len(_score_labels):
 		_score_labels[index].text = str(value)
-	else:
-		push_warning("Index out of range")
+	elif OS.has_feature("debug"):
+		push_warning("Index out of range. There are only " + str(len(_score_labels)) + " score labels.")
