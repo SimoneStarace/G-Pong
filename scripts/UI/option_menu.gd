9ad4c2a21521cd8f094a9aa3e8d2e67844178e2a
Simone Starace <simone.starace93@gmail.com>
2024-02-20 22:44:38 +0100

    Add comments to option classes

--- scripts/UI/option_menu.gd
+++ scripts/UI/option_menu.gd
@@ -1,10 +1,16 @@
 class_name OptionMenu extends Control
 
+## Reference to the spin box to assing the maximum score.
 @onready var _spin_box_score:SpinBox = $Options/ScoreOption/SpinBox
+## Reference to the list of difficulties.
 @onready var _difficulty_list:OptionButton = $Options/DifficultyOption/OptionButton
+## Reference to the color picker of the left paddle.
 @onready var _left_color_picker:ColorPickerButton = $Options/LeftPaddleOption/ColorPickerButton
+## Reference to the color picker of the right paddle.
 @onready var _right_color_picker:ColorPickerButton = $Options/RightPaddleOption/ColorPickerButton
+## Reference to the color picker of the ball.
 @onready var _ball_color_picker:ColorPickerButton = $Options/BallOption/ColorPickerButton
+## Reference to the color picker of the background.
 @onready var _background_color_picker:ColorPickerButton = $Options/BackgroundOption/ColorPickerButton
 
 func _ready():
--- scripts/managers/option_manager.gd
+++ scripts/managers/option_manager.gd
@@ -1,9 +1,15 @@
 extends Node
 
+## Maximum amount of points to score to finish the game session.
 @export_range(1,100) var _max_score:int = 10
+## The Difficulty for the A.I.
 @export_enum("Easy", "Normal", "Hard") var _difficulty:int = 1
 @export_category("Colors")
+## Color for the left paddle.
 @export_color_no_alpha var _left_paddle_color:Color = Color.WHITE
+## Color for the right paddle.
 @export_color_no_alpha var _right_paddle_color:Color = Color.WHITE
+## Color for the ball.
 @export_color_no_alpha var _ball_color:Color = Color.WHITE
+## Color for the background.
 @export_color_no_alpha var _background_color:Color = Color.BLACK
