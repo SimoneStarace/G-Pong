c0481fa0affca865b0eb6e11848551d751740ee6
Simone Starace <simone.starace93@gmail.com>
2024-02-24 14:49:23 +0100

    Add ball sound effect

--- scripts/entities/ball.gd
+++ scripts/entities/ball.gd
@@ -8,8 +8,13 @@ const _initial_speed:int = 300
 @onready var _2d_notifier:VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 ## Reference to the size of the viewport
 @onready var _center_screen:Vector2 = get_viewport_rect().size / 2
+## Sound effect of the ball.
+@onready var _sfx:AudioStreamPlayer = $SoundEffectStream
 ## Direction of the ball
 var _direction:Vector2 = Vector2.RIGHT
+
+func _ready():
+	self.area_entered.connect(collision_sound)
 
 func _process(delta):
 	# Change the position of the ball
@@ -19,3 +24,8 @@ func _process(delta):
 func reset():
 	global_position = _center_screen
 	_speed = _initial_speed
+
+## Makes the sound when the it collides to a wall or a paddle
+func collision_sound(area):
+	if _sfx.stream != null and (area is Paddle or area is Wall):
+		_sfx.play()
