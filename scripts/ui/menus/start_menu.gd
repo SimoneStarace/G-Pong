d2650a5639b6e9cc6cec488c04dbf12d5dc2c9fb
Simone Starace <simone.starace93@gmail.com>
2024-02-25 23:42:49 +0100

    Update main menu with new button

--- scripts/ui/menus/start_menu.gd
+++ scripts/ui/menus/start_menu.gd
@@ -1,4 +1,10 @@
 class_name StartMenu extends Control
 
-func _on_play_button_pressed():
+func load_game_scene():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_player_button_pressed():
+	load_game_scene()
+
+func _on_ai_button_pressed():
+	load_game_scene()
