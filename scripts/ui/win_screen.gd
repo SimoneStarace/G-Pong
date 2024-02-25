4e4a4278d426d6f510bbc77055df0de2f5e2d743
Simone Starace <simone.starace93@gmail.com>
2024-02-25 01:40:02 +0100

    Update win screen scene and script

--- scripts/ui/win_screen.gd
+++ scripts/ui/win_screen.gd
@@ -1,15 +1,12 @@
 class_name WinScreen extends Control
 
 ## Label to show the winner player
-@onready var _winner_label:Label = $WinnerLabel
+@onready var _winner_label:Label = $MarginContainer/WinnerLabel
 
 func _ready():
 	# Connect the button signals with lambda functions
 	$ReplayButton.pressed.connect(func():
 		get_tree().reload_current_scene()
-	)
-	$MenuButton.pressed.connect(func():
-		get_tree().change_scene_to_file("res://scenes/UI/start_menu.tscn")
 	)
 
 ## Sets the winner label text
