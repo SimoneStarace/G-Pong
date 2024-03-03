3a0bc46323fc883427027bc1ff8153c931fbebcd
Simone Starace <simone.starace93@gmail.com>
2024-03-03 13:01:07 +0100

    Make game unpaused when start menu is loaded

--- scripts/ui/menus/start_menu.gd
+++ scripts/ui/menus/start_menu.gd
@@ -1,5 +1,10 @@
 class_name StartMenu extends Control
 
+func _ready():
+	# Make the game unpaused if it was paused during the game
+	get_tree().paused = false
+
+## Loads the game scene
 func load_game_scene():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
